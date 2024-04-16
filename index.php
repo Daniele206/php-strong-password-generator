556588c46ae636add78dc94cd6e3fb01c0211080
Daniele206 <danieledeieso2004@gmail.com>
2024-04-16 17:18:07 +0200

    [fix] sintax

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 require __DIR__ . '/data/data.php';
 require __DIR__ . '/functions/random_pass.php';
 
-$pass_length = $_GET['pass_lenght'];
+$pass_length = $_GET['pass_length'];
 
 $array_pass = random_pass($pass_length, $letters, $numbers, $symbols);
 
 $pass = implode('', $array_pass);
-
 
 ?>
 
@@ -20,7 +19,7 @@ $pass = implode('', $array_pass);
     <form class="pt-5" action="index.php" method="GET">
     <h3>Inserire la lunghezza della password</h3>
     <div class="input-group input-group-lg w-50">
-      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" name="pass_lenght">
+      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" name="pass_length">
     </div>
     <button class="btn btn-primary mt-4">Genera</button>
     </form>
