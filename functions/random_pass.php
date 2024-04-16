fb9f3c1ed126383a7c783e45ce9b84fbc24167a5
Daniele206 <danieledeieso2004@gmail.com>
2024-04-16 16:57:44 +0200

    [add] function

---
+++ functions/random_pass.php
@@ -0,0 +1,23 @@
+<?php
+include __DIR__ . '../data/data.php';
+
+function random_pass($pass_length){
+  // for($i = 0; $i < $pass_length; $i++){
+  //   $pass = [];
+  //   $l_n_s = rand(1, 3);
+  //   if($l_n_s === 1){
+  //     $l_r = rand(0, count($letters));
+  //     $pass = $letters[$l_r];
+  //   }elseif($l_n_s === 2){
+  //     $n_r = rand(0, count($numbers));
+  //     $pass = $numbers[$n_r];
+  //   }elseif($l_n_s === 3){
+  //     $s_r = rand(0, count($symbols));
+  //     $pass = $symbols[$s_r];
+  //   };
+  //   return $pass
+  // }
+  return 'ciao';
+}
+
+?>
