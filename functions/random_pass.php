c39a26d5367d767915be66c403ad3aea3d2c8835
Daniele206 <danieledeieso2004@gmail.com>
2024-04-16 16:59:28 +0200

    [add] function e data whit require

--- functions/random_pass.php
+++ functions/random_pass.php
@@ -1,23 +1,22 @@
 <?php
 include __DIR__ . '../data/data.php';
 
-function random_pass($pass_length){
-  // for($i = 0; $i < $pass_length; $i++){
-  //   $pass = [];
-  //   $l_n_s = rand(1, 3);
-  //   if($l_n_s === 1){
-  //     $l_r = rand(0, count($letters));
-  //     $pass = $letters[$l_r];
-  //   }elseif($l_n_s === 2){
-  //     $n_r = rand(0, count($numbers));
-  //     $pass = $numbers[$n_r];
-  //   }elseif($l_n_s === 3){
-  //     $s_r = rand(0, count($symbols));
-  //     $pass = $symbols[$s_r];
-  //   };
-  //   return $pass
-  // }
-  return 'ciao';
-}
+function random_pass($pass_length, $letters, $numbers, $symbols){
+  $pass = [];
+  for($i = 0; $i < $pass_length; $i++){
+    $l_n_s = rand(1, 3);
+    if($l_n_s === 1){
+      $l_r = rand(0, (count($letters)- 1));
+      array_push($pass, $letters[$l_r]);
+    }elseif($l_n_s === 2){
+      $n_r = rand(0, (count($numbers)- 1));
+      array_push($pass, $numbers[$n_r]);
+    }elseif($l_n_s === 3){
+      $s_r = rand(0, (count($symbols)- 1));
+      array_push($pass, $symbols[$s_r]);
+    };
+  }
+  return $pass;
+};
 
 ?>
--- index.php
+++ index.php
@@ -1,95 +1,9 @@
 <?php
 
-// require __DIR__ . '/data/data.php';
-// require __DIR__ . '/functions/random_pass.php';
-
-$letters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-
-$numbers = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9'
-];
-
-$symbols = [
-  '!',
-  '?',
-  '&',
-  '%',
-  '$',
-  '<',
-  '>',
-  '^',
-  '+',
-  '-',
-  '*',
-  '/',
-  '(',
-  ')',
-  '[',
-  ']',
-  '{',
-  '}',
-  '@',
-  '#',
-  '_',
-  '='
-];
+require __DIR__ . '/data/data.php';
+require __DIR__ . '/functions/random_pass.php';
 
 $pass_length = 8;
-
-function random_pass($pass_length, $letters, $numbers, $symbols){
-  $pass = [];
-  for($i = 0; $i < $pass_length; $i++){
-    $l_n_s = rand(1, 3);
-    if($l_n_s === 1){
-      $l_r = rand(0, (count($letters)- 1));
-      array_push($pass, $letters[$l_r]);
-    }elseif($l_n_s === 2){
-      $n_r = rand(0, (count($numbers)- 1));
-      array_push($pass, $numbers[$n_r]);
-    }elseif($l_n_s === 3){
-      $s_r = rand(0, (count($symbols)- 1));
-      array_push($pass, $symbols[$s_r]);
-    };
-  }
-  return $pass;
-};
-
 
 var_dump(random_pass($pass_length, $letters, $numbers, $symbols));
 
@@ -101,6 +15,6 @@ $array_pass = random_pass($pass_length, $letters, $numbers, $symbols);
 <?php include __DIR__ . '/partials/head.php' ?>
 
 <body>
-  
+
 </body>
 </html>
